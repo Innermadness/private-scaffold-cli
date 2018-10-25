c799cdf850b78dea89a1598baed54a0df21ccbaf
Innermadness <380361138@qq.com>
2018-10-25 23:09:17 +0800

    hello world

---
+++ bin/index.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+// ↑ 这句话告诉系统，使用node.js来运行本脚本（env表示会从环境变量中查找node的位置）。
+
+// npm link
+// 可以将任意位置的npm包链接到全局环境，在任意位置使用命令行都能直接运行该npm包。
+
+// const program = require('commander');
+console.log('Hello, world~~');
